7fb2e274a0cdf6cf70e72b324fde4617e1243ba9
AJXD2 <aj@ajxd2.dev>
2024-10-10 12:55:55 -0400

    Start the rewrite

--- diveharder/api_client.py
+++ diveharder/api_client.py
@@ -4,13 +4,7 @@ from time import time
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
-from diveharder.api.campaings import Campaigns
-from diveharder.api.dispatches import Dispatches
-from diveharder.api.planets import Planets
-from diveharder.api.stats import Statistics
-from diveharder.api.status import StatusAPI
-from diveharder.api.updates import UpdatesAPI
-from diveharder.api.war_info import WarInfoAPI
+
 from diveharder.constants import OFFICIAL_DIVEHARDER_URL
 
 
@@ -51,49 +45,6 @@ class DiveHarderApiClient:
         self._session.mount("https://", retry_adapter(backoff_factor, retry_count, []))
         self._user_agent = user_agent
         set_logger(debug)
-
-    @property
-    def dispatches(self) -> Dispatches:
-        return Dispatches(self, self._session, self._url, self._user_agent)
-
-    @property
-    def planets(self) -> Planets:
-        return Planets(self, self._session, self._url, self._user_agent)
-
-    @property
-    def status(self) -> StatusAPI:
-        return StatusAPI(self, self._session, self._url, self._user_agent)
-
-    @property
-    def statistics(self) -> Statistics:
-        return Statistics(self, self._session, self._url, self._user_agent)
-
-    @property
-    def war_info(self) -> WarInfoAPI:
-        return WarInfoAPI(self, self._session, self._url, self._user_agent)
-
-    @property
-    def updates(self) -> UpdatesAPI:
-        return UpdatesAPI(self, self._session, self._url, self._user_agent)
-
-    @property
-    def campaigns(self) -> Campaigns:
-        return Campaigns(self, self._session, self._url, self._user_agent)
-
-    @property
-    def game_time(self) -> float:
-        return self.status.get_status().time
-
-    def fix_timestamp(
-        self, timestamp: float, as_datetime: bool = False
-    ) -> float | datetime:
-        """Fixes the provided timestamp based on the current time."""
-
-        fixed_timestamp = (time() - self.game_time) + (timestamp + self.game_time)
-
-        return (
-            datetime.fromtimestamp(fixed_timestamp) if as_datetime else fixed_timestamp
-        )
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(url={self._url})"
